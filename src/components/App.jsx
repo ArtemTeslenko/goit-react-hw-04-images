a5d2d31c607668ff1d0b95dbc1c467f92ece3111
Artem <104884717+ArtemTeslenko@users.noreply.github.com>
2022-11-30 23:21:53 +0200

    refactor App using hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,53 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Searchbar from './Searchbar';
 import Gallery from './ImageGallery';
 import ShowModal from './Modal';
 
-class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    showModal: false,
-    largeImgData: { src: '', alt: '' },
-    isQueryChanged: false,
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImgData, setLargeImgData] = useState({ src: '', alt: '' });
+  const [isQueryChanged, setisQueryChanged] = useState(false);
+
+  const handleSubmit = newQuery => {
+    setQuery(newQuery);
+    setPage(1);
+    setisQueryChanged(true);
   };
 
-  setQuery = query => {
-    this.setState({ query: query, page: 1, isQueryChanged: true });
+  const shareSrc = (src, alt) => {
+    setLargeImgData({ src, alt });
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const loadMore = () => {
+    setisQueryChanged(false);
+    setPage(prev => prev + 1);
   };
 
-  shareSrc = (src, alt) => {
-    this.setState({ largeImgData: { src, alt } });
-  };
-
-  loadMore = () => {
-    this.setState(prev => ({
-      isQueryChanged: false,
-      page: (prev.page += 1),
-    }));
-  };
-
-  render() {
-    const { largeImgData, showModal, query, page, isQueryChanged } = this.state;
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.setQuery} />
-        <Gallery
-          query={query}
-          page={page}
-          onImgClick={this.toggleModal}
-          shareSrc={this.shareSrc}
-          loadMore={this.loadMore}
-          isQueryChanged={isQueryChanged}
-        />
-        {showModal && (
-          <ShowModal onClose={this.toggleModal}>
-            <img src={largeImgData.src} alt={largeImgData.alt} />
-          </ShowModal>
-        )}
-        <ToastContainer
-          autoClose={2000}
-          hideProgressBar={true}
-          theme={'colored'}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleSubmit} />
+      <Gallery
+        query={query}
+        page={page}
+        onImgClick={() => setShowModal(prev => !prev)}
+        shareSrc={shareSrc}
+        loadMore={loadMore}
+        isQueryChanged={isQueryChanged}
+      />
+      {showModal && (
+        <ShowModal onClose={() => setShowModal(prev => !prev)}>
+          <img src={largeImgData.src} alt={largeImgData.alt} />
+        </ShowModal>
+      )}
+      <ToastContainer
+        autoClose={2000}
+        hideProgressBar={true}
+        theme={'colored'}
+      />
+    </div>
+  );
 }
-
-export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -56,9 +56,11 @@ export default function Gallery({
       .finally(() => {
         setIsLoading(false);
       });
-
-    autoscroll();
   }, [query, page, isQueryChanged]);
+
+  useEffect(() => {
+    autoscroll();
+  }, [images]);
 
   return (
     <>
@@ -97,102 +99,11 @@ export default function Gallery({
   );
 }
 
-// class OldGallery extends Component {
-//   state = {
-//     images: [],
-//     error: null,
-//     isLoading: false,
-//     totalHits: 0,
-//   };
-
-//   COMMON_URL = 'https://pixabay.com/api/';
-//   KEY = '30103302-a3ef06cdfdc78e2e196d775c9';
-
-//   componentDidUpdate(prevProps, _) {
-//     const { query, page } = this.props;
-
-//     if (prevProps.query !== query) {
-//       this.setState({
-//         images: [],
-//       });
-//     }
-
-//     if (prevProps.page !== page || prevProps.query !== query) {
-//       this.setState({
-//         isLoading: true,
-//       });
-
-//       fetch(
-//         `${this.COMMON_URL}?key=${this.KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`
-//       )
-//         .then(response => {
-//           if (response.ok) {
-//             return response.json();
-//           }
-//           return Promise.reject(
-//             new Error('Search error, please put the correct value.')
-//           );
-//         })
-//         .then(data =>
-//           this.setState(previous => ({
-//             images: [...previous.images, ...data.hits],
-//             isLoading: false,
-//             totalHits: data.totalHits,
-//           }))
-//         )
-//         .catch(error => this.setState({ error }));
-//     }
-//     autoscroll();
-//   }
-
-//   onLoadMoreClick = () => {
-//     this.props.loadMore();
-//   };
-
-//   render() {
-//     const { images, isLoading, error, totalHits } = this.state;
-//     const { onImgClick, shareSrc } = this.props;
-//     return (
-//       <>
-//         {error && <p>{error}</p>}
-//         {images.length < 1 && !isLoading ? (
-//           <Urging>Start searching.</Urging>
-//         ) : null}
-//         {images && (
-//           <div className="gallery">
-//             <ImageGallery>
-//               {images &&
-//                 images.map(({ id, tags, webformatURL, largeImageURL }) => {
-//                   return (
-//                     <GalleryItem
-//                       key={id}
-//                       alternative={tags}
-//                       src={webformatURL}
-//                       largSrc={largeImageURL}
-//                       onImgClick={onImgClick}
-//                       shareSrc={shareSrc}
-//                     />
-//                   );
-//                 })}
-//             </ImageGallery>
-//           </div>
-//         )}
-//         {!isLoading && images.length > 0 && images.length < totalHits ? (
-//           <LoadMore onLoadMoreClick={this.onLoadMoreClick}>Load more</LoadMore>
-//         ) : null}
-//         {isLoading && (
-//           <Loader>
-//             <ThreeDots color="#3f51b5" ariaLabel="three-dots-loading" />
-//           </Loader>
-//         )}
-//       </>
-//     );
-//   }
-// }
-
 Gallery.propTypes = {
   shareSrc: PropTypes.func.isRequired,
   onImgClick: PropTypes.func.isRequired,
   query: PropTypes.string.isRequired,
+  page: PropTypes.number.isRequired,
   loadMore: PropTypes.func.isRequired,
+  isQueryChanged: PropTypes.bool.isRequired,
 };
